0892be629ae7754c13f98895579f08a53548fa60
João Victor Piacenza de Oliveira Andrade <joaovictorpiacenza@gmail.com>
2025-05-29 09:49:31 -0300

    Update horario.py

--- horario.py
+++ horario.py
@@ -6,9 +6,17 @@ def janela_horario():
     janela = tk.Tk() 
     janela.title("Horário")
     janela.geometry("300x200")
+    largura_janela = 300
+    altura_janela = 200
+    largura_tela = janela.winfo_screenwidth()
+    altura_tela = janela.winfo_screenheight()
+    x = largura_tela - largura_janela
+    y = altura_tela - altura_janela
+    janela.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
+    janela.attributes("-topmost", True)
 
     hora = tk.Label(janela, text= "Horario de Brasília: ", font=("Arial",20))
-    hora.pack(pady=10) 
+    hora.pack(pady=5) 
 
     def fechar_janela():
         janela.destroy() 
